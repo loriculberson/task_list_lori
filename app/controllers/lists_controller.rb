c1295d736d49e58ba5b7d8e2f8c91ceaa4e8a406
Lori Culberson <bigskybandit@sbcglobal.net>
2015-08-11 12:15:08 -0600

    List crud complete

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,9 +17,28 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
 
   private
   def list_params
-    params.require(:list).permit(:title)
+    params.require(:list).permit(:title, :description)
   end
 end
