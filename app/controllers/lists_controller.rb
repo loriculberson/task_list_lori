d51aa65ab075d1c8aa1719151891b020e61bfb1e
Lori Culberson <bigskybandit@sbcglobal.net>
2015-08-11 11:29:37 -0600

    List must have title. Does not have to have a description

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,6 +20,6 @@ class ListsController < ApplicationController
 
   private
   def list_params
-    params.require(:list).permit(:title, :description)
+    params.require(:list).permit(:title)
   end
 end
