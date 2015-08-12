0fe305b162739de7c97d078d1a95dedc6b1e46f8
Lori Culberson <bigskybandit@sbcglobal.net>
2015-08-12 12:54:49 -0600

    WIP trying to fix js error and heroku not allowing upload

--- app/assets/javascripts/sorting.js
+++ app/assets/javascripts/sorting.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
   var table = $('.tasks').DataTable( {
-      "columnDefs": [
-        { "orderable": false, "targets": [5,6] }
-      ]
+    { "orderable": false, "targets": [5,6] }
   });
 });
